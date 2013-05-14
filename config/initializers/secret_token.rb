889083ef03713f47a01c5ab818d25e9110e77b19
waterlink000 (Alexey F.) <waterlink000@gmail.com>
2013-05-14 23:28:56 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cardvi::Application.config.secret_token = 'cf8b76a01bdf6fd5776371e598cc760c1752de033b6b6183f976324fbb9b3fb700542d5390d16e505ac66e4fea215ebb5394976d29e28e9006f234d58383de0b'
